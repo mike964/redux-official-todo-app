0bd8e3c2cc15acc293b034ebbf88adb40fde4f3a
Amen Kemal <57805638+mike964@users.noreply.github.com>
2022-05-03 07:04:33 +0300

    Initial commit

---
+++ src/features/header/Header.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+const Header = () => {
+  const [text, setText] = useState('')
+  const dispatch = useDispatch()
+
+  const handleChange = (e) => setText(e.target.value)
+
+  const handleKeyDown = (e) => {
+    // If the user pressed the Enter key:
+    const trimmedText = text.trim()
+    if (e.which === 13 && trimmedText) {
+      // Dispatch the "todo added" action with this text
+      dispatch({ type: 'todos/todoAdded', payload: trimmedText })
+      // And clear out the text input
+      setText('')
+    }
+  }
+
+  return (
+    <header className="header">
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+    </header>
+  )
+}
+
+export default Header
